babafc6c2f124b80fbbf5c9a186ef87f2c9c63f1
Krzem <krzem5.dev@gmail.com>
2022-07-18 14:45:24 +0200

    Push Update 18/07/2022, 14:45:24

--- build.py
+++ build.py
@@ -42,7 +42,7 @@ else:
 			for f in cfl:
 				if (f[-2:]==".c"):
 					fl.append(f"build/{(r+f).replace('/','$')}.o")
-					if (subprocess.run(["gcc","-Wall","-lm","-Werror","-mavx","-mavx2","-mbmi","-mbmi2","-O3","-g","-c",r+f,"-o",f"build/{(r+f).replace('/','$')}.o","-Isrc/include"]).returncode!=0):
+					if (subprocess.run(["gcc","-Wall","-lm","-Werror","-mavx","-mavx2","-mbmi","-mbmi2","-O3","-c",r+f,"-o",f"build/{(r+f).replace('/','$')}.o","-Isrc/include"]).returncode!=0):
 						sys.exit(1)
 		if (subprocess.run(["gcc","-mavx","-mavx2","-mbmi","-mbmi2","-o","build/wave_function_collapse"]+fl).returncode!=0):
 			sys.exit(1)
