66bee9760297063693a73dcf3a84ce03758437bb
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-02-24 21:18:16 +0100

    Push Update 24/02/2023, 21:18:16

--- src/main.c
+++ src/main.c
@@ -14,6 +14,8 @@
 
 
 
+#define USE_PRECALCULATED_MASKS 1
+
 #define PICK_PARAMETERS 0
 #define GENERATE_IMAGE 1
 #define IMAGE_NAME "cow"
@@ -126,10 +128,12 @@ int main(int argc,const char** argv){
 	output_image.width/=table.downscale_factor;
 	output_image.height/=table.downscale_factor;
 	wfc_state_t state;
-	wfc_init_state(&table,&output_image,1,&state);
-	fflush(stdout);
+	time_start=get_time();
+	wfc_init_state(&table,&output_image,USE_PRECALCULATED_MASKS,&state);
+	unsigned long int state_creation_time=get_time()-time_start;
 	time_start=get_time();
 	wfc_stats_t stats;
+	fflush(stdout);
 	wfc_solve(&table,&state,&config,_progress_callback,&output_image,&stats);
 	printf("\x1b[0m\x1b[?25h\x1b[%uA",output_image.height);
 	unsigned long int generation_time=get_time()-time_start;
@@ -141,7 +145,7 @@ int main(int argc,const char** argv){
 	wfc_generate_full_scale_image(&table,&state,&output_image);
 	putchar('\n');
 	wfc_print_image(&output_image);
-	printf("Table:\n  Tile count: %s\n  Tile element size: %s B\n  Memory use:\n    Tile data: %s kB\n    Neighbours: %s kB\n    Upscaled data: %s kB\nTable creation time: %.3lfs\nSimulation state:\n  Bitmap: %s kB\n  Pixel tile data: %s kB\n  Queues: %s kB\n  Weights: %s kB\n  Stacks: %s kB\n  Cache: %s kB\n  Precalculated masks: %s kB\nSimulation:\n  Updates:\n    Collapse: %s\n    Propagation: %s\n  Removals:\n    Pixels: %s\n    Restarts: %s\n  Data access:\n",
+	printf("Table:\n  Tile count: %s\n  Tile element size: %s B\n  Memory use:\n    Tile data: %s kB\n    Neighbours: %s kB\n    Upscaled data: %s kB\nTable creation time: %.3lfs\nSimulation state:\n  Bitmap: %s kB\n  Pixel tile data: %s kB\n  Queues: %s kB\n  Weights: %s kB\n  Stacks: %s kB\n  Cache: %s kB\n  Precalculated masks: %s kB\nSimulation state creation time: %.3lfs\nSimulation:\n  Updates:\n    Collapse: %s\n    Propagation: %s\n  Removals:\n    Pixels: %s\n    Restarts: %s\n  Data access:\n",
 		_format_int(table.tile_count),
 		_format_int(table.data_elem_size*sizeof(uint64_t)),
 		_format_int((table.tile_count*config.box_size*config.box_size*sizeof(wfc_color_t)+1023)>>10),
@@ -155,6 +159,7 @@ int main(int argc,const char** argv){
 		_format_int((2*state.pixel_count*sizeof(wfc_size_t)+1023)>>10),
 		(!state.precalculated_masks?_format_int((262208*sizeof(wfc_fast_mask_t)+1023)>>10):"0"),
 		(state.precalculated_masks?_format_int((((table.data_elem_size-1)*(table.data_elem_size-1)*8192ul+(table.data_elem_size-1)*1024+3*256+256)*32+1023)>>10):"0"),
+		state_creation_time*1e-9,
 		_format_int(stats.steps),
 		_format_int(stats.propagation_steps),
 		_format_int(stats.deleted_tiles),
